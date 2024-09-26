53f8c04506c1152d2e5b52a1a8abd2350c050dc9
TonyFreddy <kounleadissa@gmail.com>
2024-09-26 11:46:59 +0200

    bug fixed

--- backend/src/controllers/workflow_creation.js
+++ backend/src/controllers/workflow_creation.js
@@ -33,33 +33,5 @@ const create_workflow = async (req, res) => {
     //     res.status(400).json({ message: "Erreur lors de la création du workflow" });
     // }
 
-
-// Fonction pour changer le statut d'un workflow
-const updateWorkflowStatus = async (workflowId, newStatus) => {
-    try {
-        const workflow = await Workflow.findById(workflowId);
-        if (!workflow) throw new Error('Workflow non trouvé');
-        
-        workflow.status = newStatus; // Mettre à jour le statut
-        await workflow.save();
-        
-        return { success: true, message: `Le statut du workflow a été mis à jour en '${newStatus}'` };
-    } catch (error) {
-        console.error('Erreur lors de la mise à jour du statut du workflow:', error.message);
-        return { success: false, error: error.message };
-    }
-};
-
-// Fonctions pour fermer ou annuler le workflow
-const closeWorkflow = async (workflowId) => {
-    return await updateWorkflowStatus(workflowId, 'closed');
-};
-
-const cancelWorkflow = async (workflowId) => {
-    return await updateWorkflowStatus(workflowId, 'canceled');
-};
-
-module.exports = { create_workflow, closeWorkflow, cancelWorkflow };
-};
-
+}
 module.exports = { create_workflow };
